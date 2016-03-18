a8187e6f6cccf7f4fcbbf605dfe9fdd8bfc9ce8b
Valentyn Klindukh <valikk@gmail.com>
2016-03-18 09:41:28 +0000

    cleanup

--- pacemaker-PG-HA/check.sh
+++ pacemaker-PG-HA/check.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker exec postgresql1 su postgres -c 'createdb -h 172.28.0.100 test_db'
-docker exec postgresql1 su postgres -c 'psql -h 172.28.0.100 test_db -c "CREATE TABLE test_table (id int, name text)"'
-docker exec postgresql1 su postgres -c 'psql -h 172.28.0.100 test_db -c "INSERT INTO test_table (id) VALUES (1)"'
-docker exec postgresql3 su postgres -c 'psql test_db -c "SELECT * FROM test_table"'
+docker exec pg1 su postgres -c 'createdb -h 172.28.0.100 test_db'
+docker exec pg1 su postgres -c 'psql -h 172.28.0.100 test_db -c "CREATE TABLE test_table (id int, name text)"'
+docker exec pg1 su postgres -c 'psql -h 172.28.0.100 test_db -c "INSERT INTO test_table (id) VALUES (1)"'
+docker exec pg3 su postgres -c 'psql test_db -c "SELECT * FROM test_table"'
 
 
-# docker exec postgresql1 crm_mon -Afr -1
-# docker exec postgresql1 crm resource cleanup msPostgresql
-# docker exec postgresql1 corosync-cfgtool -R
+# docker exec pg1 crm_mon -Afr -1
+# docker exec pg1 crm resource cleanup msPostgresql
+# docker exec pg1 corosync-cfgtool -R
--- pacemaker-PG-HA/postgresql/repl_init.sh
+++ pacemaker-PG-HA/postgresql/repl_init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#while ! ssh postgresql1 "psql -c \"SELECT pg_start_backup('initial_backup');\""; do
-#    echo 'waiting for postgresql1 to be ready'
+#while ! ssh pg1 "psql -c \"SELECT pg_start_backup('initial_backup');\""; do
+#    echo 'waiting for pg1 to be ready'
 #    sleep 3
 #done
 
-ssh postgresql1 "rsync -a /var/lib/postgresql/9.5/main/ `hostname`:/var/lib/postgresql/9.5/main/ --exclude  postmaster.pid"
-#ssh postgresql1 "psql -c \"SELECT pg_stop_backup();\""
+ssh pg1 "rsync -a /var/lib/postgresql/9.5/main/ `hostname`:/var/lib/postgresql/9.5/main/ --exclude  postmaster.pid"
+#ssh pg1 "psql -c \"SELECT pg_stop_backup();\""
